808a22d76415e849a2035162c90e52bc3e59221d
Sahil Khatiwada <sahilkhatiwada@hotmail.com>
2024-12-04 18:32:03 +0545

    minor change

--- server/database/dbConnect.js
+++ server/database/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URl);
         console.log(`MongoDB connected`);
     } catch (err) {
         console.log(err);
